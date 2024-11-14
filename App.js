a0907ee20614b438e11269b0e9b97d13baef0d6b
pmoraes230 <pmoraes230nascimento@gmail.com>
2024-11-14 16:43:31 -0300

    ligando arquivos js diferentes no app

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Teste from './src/components/index'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Olha pra mim!! ass: Melhor aluno do Diego.</Text>
+      <Teste/>
     </View>
   );
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,20 +1,10 @@
 import React from "react";
 import { View, Text } from "react-native";
 
-export default function title() {
+export default function Teste() {
     return (
-      <View style={styles.container}>
-        <Text>Olha pra mim!! ass: Melhor aluno do Diego.</Text>
+      <View>
+        <Text>Olha pra mim!! ass: Melhor aluno do Diego...</Text>
       </View>
     );
   }
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
-  
